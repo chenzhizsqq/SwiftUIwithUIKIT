e7d5e3f161edfac55961e9c75d0a31b0e7873719
chenzhizs <chenzhi@qq.com>
2021-04-14 11:17:11 +0900

    像视频那样完成了

---
+++ NewSwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  NewSwiftUIView.swift
+//  SwiftUIwithUIKIT
+//
+//  Created by chenzhizs on 2021/04/14.
+//
+
+import SwiftUI
+
+struct NewSwiftUIView: View {
+    var body: some View{
+        Text("Hello SwiftUI")
+    }
+    
+}
+
+struct NewSwiftUIView_Previews : PreviewProvider {
+    static var previews : some View {
+        NewSwiftUIView()
+    }
+}
